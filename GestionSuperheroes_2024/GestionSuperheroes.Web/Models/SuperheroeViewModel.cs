b7402fe36860a3b77ba943b8e41a60e05ebeb2a5
Mario <mario.checchia@outlook.com>
2025-11-05 14:21:28 -0300

    Se agregaron validaciones a gestion de superheroes

--- GestionSuperheroes_2024/GestionSuperheroes.Web/Models/SuperheroeViewModel.cs
+++ GestionSuperheroes_2024/GestionSuperheroes.Web/Models/SuperheroeViewModel.cs
@@ -1,9 +1,17 @@
-﻿namespace GestionSuperheroes.Web.Models
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace GestionSuperheroes.Web.Models
 {
     public class SuperheroeViewModel
     {
+        [Required(ErrorMessage = "El nombre es obligatorio.")]
+        [StringLength(100, MinimumLength = 2, ErrorMessage = "El nombre debe tener entre 2 y 100 caracteres.")]
+        [RegularExpression("^[A-Za-zÁÉÍÓÚáéíóúÑñ ]+$", ErrorMessage = "Solo se permiten letras y espacios.")]
         public string NombreSuperheroe { get; set; }
+
+        [Required(ErrorMessage = "Debe seleccionar un universo.")]
         public int IdUniverso { get; set; }
+
         public int IdSuperheroe { get; set; }
     }
 }
